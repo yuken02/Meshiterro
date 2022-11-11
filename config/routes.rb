2613e4064669f511ab16f6003a80725b926692b2
yuken02 <git@github.com:yuken02/practice.git>
2022-11-11 09:30:24 +0000

    [Add]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   get '/homes/about', to: 'homes#about', as: 'about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
