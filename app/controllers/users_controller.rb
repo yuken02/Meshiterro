f4075262513e270d9ed17258d5957a4518a015f4
yuken02 <git@github.com:yuken02/practice.git>
2022-11-14 04:05:56 +0000

    [Add]4-25 アクセス制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
@@ -9,15 +11,23 @@ class UsersController < ApplicationController
   end
 
   def update
-    # @user = User.find(params[:id])
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(current_user.id)
   end
+
 
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if (user_id != login_user_id)
+      redirect_to post_images_path
+    end
   end
 end
